c83aacf12258f350c0b6dd0b54fc797f80e94690
harshitScript <85029089+harshitScript@users.noreply.github.com>
2021-09-17 15:19:38 +0530

    media query improvements

--- Components/MainPageComponents/ContactMeForm.js
+++ Components/MainPageComponents/ContactMeForm.js
@@ -17,7 +17,7 @@ const ContactMeForm = () => {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [displayFormResponseObj, setDisplayFormResponseObj] = useState({
-    displayFormResponse: false,
+    displayFormResponse: true,
     responseId: "",
   });
   const {
